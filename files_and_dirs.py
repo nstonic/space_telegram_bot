05ec6ec52b7a9f13d507d6f2abe9688daec6ad88
nstonic <118763806+nstonic@users.noreply.github.com>
2023-01-15 18:43:58 +0400

    make bot regular posting photos

--- files_and_dirs.py
+++ files_and_dirs.py
@@ -1,6 +1,7 @@
 import os
 from urllib.parse import urlparse, unquote
 import requests
+from PIL import Image
 
 
 def get_file_ext(url: str) -> str:
@@ -17,3 +18,9 @@ def download_image(url: str, target_path: str):
     with open(target_path, "wb") as file:
         file.write(image_responce.content)
     print("Image downloaded:", url)
+
+
+def resize_image(image_file: str, long_side: int = 2048):
+    image = Image.open(image_file)
+    image.thumbnail((long_side, long_side))
+    image.save(image_file)
--- main.py
+++ main.py
@@ -1,12 +1,35 @@
+import os
+import random
+import time
+from PIL import Image
 from fetch_nasa_apod import fetch_nasa_apod
 from fetch_nasa_epic import fetch_nasa_epic
 from fetch_spacex_images import fetch_spacex_images
+import telegram
+from dotenv import load_dotenv
+from files_and_dirs import resize_image
 
 
 def main():
-    fetch_spacex_images()
-    fetch_nasa_apod(50)
-    fetch_nasa_epic(50)
+    while True:
+        fetch_spacex_images()
+        fetch_nasa_apod(10)
+        fetch_nasa_epic(10)
+
+        load_dotenv()
+        images = os.listdir("images")
+        image_file = f"images/{random.choice(images)}"
+        resize_image(image_file)
+
+        bot = telegram.Bot(token=os.getenv('TELEGRAM_BOT_API'))
+        bot.send_photo(chat_id="@nstonic_SpacePhotos", photo=open(image_file, "rb"))
+
+        if os.getenv('DELAY_SECONDS'):
+            delay = int(os.getenv('DELAY_SECONDS'))
+        else:
+            delay = 14400
+
+        time.sleep(delay)
 
 
 if __name__ == '__main__':
